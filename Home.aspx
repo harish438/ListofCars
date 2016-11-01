cc6099775e6e54f08c3403a39bf92cff0fc88afe
uday <uday.kundoor123@gmail.com>
2016-11-01 16:07:11 -0400

    ListofCars

---
+++ Home.aspx
@@ -0,0 +1,76 @@
+﻿<<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="SampleApp.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" style="margin-left:30px">
+<head runat="server">
+    <title>List of Cars</title>
+    <style type="text/css">
+        .auto-style1 {
+            text-decoration: underline;
+        }
+    </style>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
+    <script src="scripts/bootstrap.min.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p>1.Click the button to get the <span class="auto-style1"><strong>Newest vechicles</strong></span> in order&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:Button ID="Button1" class="btn btn-primary" runat="server" Text="Newest Vehicles" OnClick="Button1_Click" /></p>
+        
+        <p>2. Click the button to get <span class="auto-style1"><strong>Alphabetized List of vehicles</strong></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:Button ID="Button2" class="btn btn-success" runat="server" Text="Alphabetized List" OnClick="Button2_Click" /></p>
+        
+        <p>3.Click the button to get <strong><span class="auto-style1">Ordered List of vehicles by Price</span>&nbsp;&nbsp;&nbsp;&nbsp; </strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:Button ID="Button3" class="btn btn-info" runat="server" Text="Ordered List By Price" OnClick="Button3_Click" /></p>
+        
+        <p>4. Click the button to <strong><span class="auto-style1">Get the best value vehicle with Low Price &amp; Good mpg</span>&nbsp; </strong>&nbsp; <asp:Button ID="Button4" runat="server" class="btn btn-warning" Text="Best Value Car" OnClick="Button4_Click" /></p>
+        
+        <p>5. <span class="auto-style1"><strong>Fuel consumption based on vehicle</strong></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+            <asp:DropDownList ID="DropDownList1" runat="server">
+            </asp:DropDownList>
+            
+            <asp:TextBox ID="TextBox2" runat="server" placeholder="Enter number of Miles"></asp:TextBox>
+            <asp:Button ID="Button7" runat="server"  class="btn btn-info" Text="Fuel Consumption" OnClick="Button7_Click" />
+            &nbsp;<asp:Label ID="Label3" runat="server"></asp:Label>
+        </p>
+        
+        <p>6. Click the button to <strong><span class="auto-style1">Get a Random car from the List</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </strong>&nbsp;<asp:Button ID="Button5" runat="server" class="btn btn-success" Text="Random car" OnClick="Button5_Click" /></p>
+        
+        <p>7. <strong><span class="auto-style1">Average mpg based on year</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </strong>&nbsp;<asp:TextBox ID="TextBox1" runat="server" placeholder="Enter year"></asp:TextBox><asp:Button ID="Button6" class="btn btn-primary" runat="server" Text="Get Average mpg" OnClick="Button6_Click" />
+            &nbsp;&nbsp;
+            <asp:Label ID="Label1" runat="server" ForeColor="Red"></asp:Label><asp:Label ID="Label2" runat="server" ForeColor="#339966"></asp:Label>
+        </p>
+        <p style="color:purple"><strong>List of Cars:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </strong></p>
+        <div>
+        <asp:GridView ID="GridView1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" Width="657px">
+            <AlternatingRowStyle BackColor="lightpink" />
+            <FooterStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#FFCC66" ForeColor="#333333" HorizontalAlign="Center" />
+            <RowStyle BackColor="#FFFBD6" ForeColor="#333333" />
+            <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="Navy" />
+            <SortedAscendingCellStyle BackColor="#FDF5AC" />
+            <SortedAscendingHeaderStyle BackColor="#4D0000" />
+            <SortedDescendingCellStyle BackColor="#FCF6C0" />
+            <SortedDescendingHeaderStyle BackColor="#820000" />
+        </asp:GridView>
+    </div>
+       <div>
+           <span style="color:purple"><strong>Ordered List o/p:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+           <asp:Label ID="Label4" runat="server" ForeColor="#003366" Text="Label"></asp:Label>
+           <br />
+           </strong></span><br />
+           <asp:GridView ID="GridView2" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" Width="656px">
+            <AlternatingRowStyle BackColor="lightblue" />
+            <FooterStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#FFCC66" ForeColor="#333333" HorizontalAlign="Center" />
+            <RowStyle BackColor="#FFFBD6" ForeColor="#333333" />
+            <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="Navy" />
+            <SortedAscendingCellStyle BackColor="#FDF5AC" />
+            <SortedAscendingHeaderStyle BackColor="#4D0000" />
+            <SortedDescendingCellStyle BackColor="#FCF6C0" />
+            <SortedDescendingHeaderStyle BackColor="#820000" />
+        </asp:GridView>
+       </div>
+       </form>
+</body>
+</html>
---
+++ Home.aspx.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+namespace SampleApp
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                var carsList = GetAllCars();
+                GridView1.DataSource = carsList;
+                GridView1.DataBind();
+                DropDownList1.DataTextField = "Make";
+                DropDownList1.DataValueField = "Id";
+                DropDownList1.DataSource = carsList;
+                DropDownList1.DataBind();
+            }
+
+        }
+
+        private IList<CarModal> GetAllCars()
+        {
+            CarModal car1 = new CarModal
+            {
+                Id = 1,
+                Make = "Honda",
+                Model = "CRV",
+                Color = "green",
+                Year = 2016,
+                Price = 23845,
+                TCCRating = 8,
+                HwyMPG = 33
+            };
+            CarModal car2 = new CarModal
+            {
+                Id = 2,
+                Make = "Ford",
+                Model = "Escape",
+                Color = "Red",
+                Year = 2017,
+                Price = 23590,
+                TCCRating = 7.8,
+                HwyMPG = 32
+            };
+            CarModal car3 = new CarModal
+            {
+                Id = 3,
+                Make = "Hyundai",
+                Model = "Santa Fe",
+                Color = "Grey",
+                Year = 2016,
+                Price = 24950,
+                TCCRating = 8,
+                HwyMPG = 27
+            };
+            CarModal car4 = new CarModal
+            {
+                Id = 4,
+                Make = "Mazda",
+                Model = "CX-5",
+                Color = "Red",
+                Year = 2017,
+                Price = 21795,
+                TCCRating = 8,
+                HwyMPG = 35
+            };
+
+            CarModal car5 = new CarModal
+            {
+                Id = 5,
+                Make = "Subaru",
+                Model = "Forester",
+                Color = "Blue",
+                Year = 2016,
+                Price = 22395,
+                TCCRating = 8.4,
+                HwyMPG = 32
+            };
+            return new List<CarModal> { car1, car2, car3, car4, car5 };
+        }
+
+        private class CarModal
+        {
+            public int Id { get; set; }
+            public string Make { get; set; }
+            public string Model { get; set; }
+            public string Color { get; set; }
+            public int Year { get; set; }
+            public int Price { get; set; }
+            public double TCCRating { get; set; }
+            public int HwyMPG { get; set; }
+        }
+
+        // To get the Newest vehicles in order from the list of cars //
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var cars = GetAllCars();
+            GridView2.DataSource = cars.OrderByDescending(x => x.Year);
+            GridView2.DataBind();
+            Label1.Text = "";
+            Label2.Text = "";
+            Label3.Text = "";
+            Label4.Text = "Below is the list of new vehicles as per year.";
+        }
+
+        // To get the Alphabetized vehicles in order from A-Z //
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            var cars = GetAllCars();
+            GridView2.DataSource = cars.OrderBy(x => x.Make);
+            GridView2.DataBind();
+            Label1.Text = "";
+            Label2.Text = "";
+            Label3.Text = "";
+            Label4.Text = "Below is the list of vehicles in Alphabetized order from A-Z.";
+        }
+
+        // To get the List of vehicles from higher price to lower price //
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            var cars = GetAllCars();
+            GridView2.DataSource = cars.OrderByDescending(x => x.Price);
+            GridView2.DataBind();
+            Label1.Text = "";
+            Label2.Text = "";
+            Label3.Text = "";
+            Label4.Text = "Below is the list of vehicles from higher price to lower price.";
+        }
+
+        // To get the Best vehicles in order from the list of cars //
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            var cars = GetAllCars();
+            GridView2.DataSource = cars.OrderByDescending(x => x.HwyMPG).ThenByDescending(x => x.TCCRating).OrderBy(x => x.Price);
+            GridView2.DataBind();
+            Label1.Text = "";
+            Label2.Text = "";
+            Label3.Text = "";
+            Label4.Text = "Below is the ordered list of vehicles with best value. The Top 1 is the best car with low cost, high mpg and good rating.";
+        }
+
+        // To get the Random car from the list of cars //
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            var cars = GetAllCars();
+            Random rnd = new Random();
+            var randomIndex = rnd.Next(1, cars.Count);
+            GridView2.DataSource = cars.Where(x => x.Id == randomIndex);
+            GridView2.DataBind();
+            Label1.Text = "";
+            Label2.Text = "";
+            Label3.Text = "";
+            Label4.Text = " Random car from the List of cars";
+        }
+
+        // To get the average mpg of vehicles for each year from the list //
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            Label4.Text = "";
+            Label3.Text = "";
+            var getYear = TextBox1.Text;
+            var cars = GetAllCars();
+            int x = 0;
+            if (Int32.TryParse(getYear, out x))
+            {
+                Label2.Text = "";
+                Label1.Text = "";
+                if (cars.Any(y => y.Year == x))
+                {
+                    double miles = cars.Where(r => r.Year == x).Average(r => r.HwyMPG);
+                    Label2.Text = "Average MPG of year " + x + " is: <b style='color:red'>" + miles + "</b>";
+                }
+                else
+                    Label1.Text = "We don't find any cars in the inventory matching the year " + x;
+            }
+            else
+            {
+                Label2.Text = "";
+                Label1.Text = "Please enter years as 2016 (or) 2017";
+            }
+
+        }
+
+        // To get the Fuel Consumption of each vehicle for a given distance in textbox //
+
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            Label1.Text = "";
+            Label2.Text = "";
+            Label4.Text = "";
+            var getDistance = TextBox2.Text;
+            double x = 0;
+            if (double.TryParse(getDistance, out x))
+            {
+                Label3.Text = "";
+                var car = GetAllCars().FirstOrDefault(y => y.Id == int.Parse(DropDownList1.SelectedValue));
+                var d = x / car?.HwyMPG;
+                if (d != null)
+                {
+                    double mpg = (double)d;
+                    Label3.Text = "Fuel consumption of <b style='color:red'>" + DropDownList1.SelectedItem + "</b> for a given distance is <b style='color:red'>" + mpg + " galon(s)</b>";
+                }
+                Label3.ForeColor = Color.Green;
+            }
+            else
+            {
+                Label3.Text = "Please enter distance in numbers";
+                Label3.ForeColor = Color.Red;
+            }
+        }
+    }
+}
